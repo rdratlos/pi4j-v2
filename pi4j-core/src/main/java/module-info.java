e5b1eeaeb8e984a3131483be2e7ce7df8e740a78
Thomas Reim <reimth@gmail.com>
2022-04-19 15:13:42 +0200

    Adding AddOnBoard plugin for Raspberry Pi expansion board projects

--- pi4j-core/src/main/java/module-info.java
+++ pi4j-core/src/main/java/module-info.java
@@ -38,6 +38,9 @@ module com.pi4j {
     exports com.pi4j.context;
     exports com.pi4j.exception;
     exports com.pi4j.extension;
+    exports com.pi4j.extension.addonboard;
+    exports com.pi4j.extension.addonboard.io;
+    exports com.pi4j.extension.addonboard.platform;
     exports com.pi4j.extension.exception;
     exports com.pi4j.event;
     exports com.pi4j.io;
@@ -59,4 +62,5 @@ module com.pi4j {
 
     // extensibility service interfaces
     uses com.pi4j.extension.Plugin;
+    uses com.pi4j.extension.addonboard.AddOnBoard;
 }
